63b061519327c722a7e624c52b588c963a36835e
Hassan <hassan.akram3282@gmail.com>
2014-04-02 21:59:36 +0500

    comment testing

--- src/com/webileapps/navdrawer/MainActivity.java
+++ src/com/webileapps/navdrawer/MainActivity.java
@@ -49,6 +49,8 @@ public class MainActivity extends SherlockFragmentActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
+		
+		//saving data from previous
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
